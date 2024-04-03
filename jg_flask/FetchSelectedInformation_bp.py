2b101f6c3154321b1d1aa4685db586285cbe133d
oThaiSan <76931895+oThaiSan@users.noreply.github.com>
2024-04-03 15:08:38 -0700

    Run it back

--- jg_flask/FetchSelectedInformation_bp.py
+++ jg_flask/FetchSelectedInformation_bp.py
@@ -1,93 +1,88 @@
 from flask import Blueprint, jsonify, request
-import requests
-import pandas as pd
-import os
-
-# Define description blueprint
-FetchSelectedInformation_bp = Blueprint('FetchSelectedInformation_bp', __name__)
-
-# Define route to handle description request
-@FetchSelectedInformation_bp.route('/process_data', methods=['POST'])
-
-# Function to fetch restaurants from Flask API
-def process_data():
-    try:
-        # Extract data received from Vue.js
-        data = request.json
-
-        # Extract relevant variables from the data
-        # These variables will differ for each type of place
-
-        # activities = data.get('activities')
-        # landmarks = data.get('landmarks')
-        foods = data.get('foods')
-        # shops = data.get('shops')
-        # hotels = data.get('hotels')
-        # datesData = data.get('datesData')
-        # budget = data.get('budget')
-        # stateData = data.get('stateData')
-        # city = data.get('city')
-        # lat = data.get('lat')
-        # long = data.get('long')
-        # cityDescription = data.get('cityDescription')
-        # citySlogan = data.get('citySlogan')
-
-
-        # print("Received data:")
-        # print("Activities:", activities)
-        # print("Landmarks:", landmarks)
-        # print("Foods:", foods)
-        # print("Shops:", shops)
-        # print("Hotels:", hotels)
-        # print("Dates Data:", datesData)
-        # print("Budget:", budget)
-        # print("State Data:", stateData)
-        # print("City:", city)
-        # print("Latitude:", lat)
-        # print("Longitude:", long)
-        # print("City Description:", cityDescription)
-        # print("City Slogan:", citySlogan)
-
-        BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-        CSV_FOLDER = os.path.join(BASE_DIR, '..', 'journey-genius-data-scraping')
-        restaurant_csv_file_path = os.path.join(CSV_FOLDER, 'restaurant_data.csv')
-
-        df = pd.read_csv(restaurant_csv_file_path, encoding='utf-8') 
-
-        for food in foods:
-            subset = df.loc[df['Place'] == food, ['Latitude', 'Longitude']]
-
-            locations = []
-            for index, row in subset.iterrows():
-                latitude = float(row['Latitude'])
-                longitude = float(row['Longitude'])
-                locations.append({'latitude': latitude, 'longitude': longitude})
-
-        response_data = {
-            'message': 'Location data retrieved successfully',
-            'locations': locations
-        }
-
-        return jsonify(response_data), 200
-
-    except Exception as e:
-        # Handle any errors that occur during processing
-        error_message = str(e)  # Convert the exception to a string
-        print("Error processing data:", error_message)
-        return jsonify({'error': 'An error occurred while processing the data', 'message': error_message})
-
-
-
-############################################# MODIFIED SHIT BELOW   :D ####################################################################
-
-
-from flask import Blueprint, jsonify, request
-import requests
 import pandas as pd
 import os
 import googlemaps
 from googlemaps.exceptions import ApiError
 from dotenv import load_dotenv
+
+
+# # Define description blueprint
+# FetchSelectedInformation_bp = Blueprint('FetchSelectedInformation_bp', __name__)
+
+# # Define route to handle description request
+# @FetchSelectedInformation_bp.route('/process_data', methods=['POST'])
+
+# # Function to fetch restaurants from Flask API
+# def process_data():
+#     try:
+#         # Extract data received from Vue.js
+#         data = request.json
+
+#         # Extract relevant variables from the data
+#         # These variables will differ for each type of place
+
+#         # activities = data.get('activities')
+#         # landmarks = data.get('landmarks')
+#         foods = data.get('foods')
+#         # shops = data.get('shops')
+#         # hotels = data.get('hotels')
+#         # datesData = data.get('datesData')
+#         # budget = data.get('budget')
+#         # stateData = data.get('stateData')
+#         # city = data.get('city')
+#         # lat = data.get('lat')
+#         # long = data.get('long')
+#         # cityDescription = data.get('cityDescription')
+#         # citySlogan = data.get('citySlogan')
+
+
+#         # print("Received data:")
+#         # print("Activities:", activities)
+#         # print("Landmarks:", landmarks)
+#         # print("Foods:", foods)
+#         # print("Shops:", shops)
+#         # print("Hotels:", hotels)
+#         # print("Dates Data:", datesData)
+#         # print("Budget:", budget)
+#         # print("State Data:", stateData)
+#         # print("City:", city)
+#         # print("Latitude:", lat)
+#         # print("Longitude:", long)
+#         # print("City Description:", cityDescription)
+#         # print("City Slogan:", citySlogan)
+
+#         BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+#         CSV_FOLDER = os.path.join(BASE_DIR, '..', 'journey-genius-data-scraping')
+#         restaurant_csv_file_path = os.path.join(CSV_FOLDER, 'restaurant_data.csv')
+
+#         df = pd.read_csv(restaurant_csv_file_path, encoding='utf-8') 
+
+#         for food in foods:
+#             subset = df.loc[df['Place'] == food, ['Latitude', 'Longitude']]
+
+#             locations = []
+#             for index, row in subset.iterrows():
+#                 latitude = float(row['Latitude'])
+#                 longitude = float(row['Longitude'])
+#                 locations.append({'latitude': latitude, 'longitude': longitude})
+
+#         response_data = {
+#             'message': 'Location data retrieved successfully',
+#             'locations': locations
+#         }
+
+#         return jsonify(response_data), 200
+
+#     except Exception as e:
+#         # Handle any errors that occur during processing
+#         error_message = str(e)  # Convert the exception to a string
+#         print("Error processing data:", error_message)
+#         return jsonify({'error': 'An error occurred while processing the data', 'message': error_message})
+
+
+
+############################################# MODIFIED SHIT BELOW   :D ####################################################################
+
 
 # Load environment variables from .env file
 load_dotenv()
